99c86027349e639d2cb5cc0bca4f14e39980ddaf
im <poson_hemp@yahoo.co.jp>
2020-11-23 07:17:29 +0900

    mix phx.gen.html

---
+++ app/my_app/lib/my_app/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule MyApp.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :bio, :string
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :bio])
+    |> validate_required([:name, :email, :bio])
+  end
+end
