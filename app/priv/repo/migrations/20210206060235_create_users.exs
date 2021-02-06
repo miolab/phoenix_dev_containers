bacd1374555c0b0dd66c2be50dd8dd45f87e0c32
im <poson_hemp@yahoo.co.jp>
2021-02-06 15:07:46 +0900

    prepare CRUD WebUI

---
+++ app/priv/repo/migrations/20210206060235_create_users.exs
@@ -0,0 +1,14 @@
+defmodule MyApp.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :bio, :string
+
+      timestamps()
+    end
+
+  end
+end
