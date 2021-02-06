7858863a41ca92f62cd341ef38bb8e96d3b7c98d
im <poson_hemp@yahoo.co.jp>
2021-02-06 15:21:25 +0900

    prepare test setting

--- app/config/test.exs
+++ app/config/test.exs
@@ -7,9 +7,9 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :my_app, MyApp.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "my_app_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
